f6113c54ced2c0086ed56721c41c11e65ed8804c
genztutorsza-creator <genztutorsza@gmail.com>
2026-05-21 01:11:53 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+    const firebaseConfig = {
+        apiKey: 'AIzaSyAGG8S44RzRP129Caj5QfyVkT8dffH0ip0',
+        authDomain: 'genztutorsza-efa13.firebaseapp.com',
+        projectId: 'genztutorsza-efa13',
+        storageBucket: 'genztutorsza-efa13.firebasestorage.app',
+        messagingSenderId: '699691673917',
+        appId: '1:699691673917:web:7ed8bd10cf0701410cfd54',
+        measurementId: 'G-CL8NS75JGJ'
+    };
